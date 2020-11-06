1429fc54548604f89874544c513a15efe498ff0d
student <solomond10@yahoo.com>
2020-11-05 23:42:50 -0700

    Regular semaphores are being used rather than bounded semaphores. Each queue has their own mutex.

--- MainProgram.py
+++ MainProgram.py
@@ -25,7 +25,6 @@ def extract(clipFileName, q1, maxFramesToLoad=9999):
 
             # add the frame to queue 1
             put(q1,image)
-            print("Q!")
             success,image = vidcap.read()
             print(f'Reading frame {count} {success}')
             count += 1
@@ -38,11 +37,13 @@ def convert(q1, q2, maxFramesToLoad=9999):
     count = 0
    
     while q1 is not None and count < maxFramesToLoad:
-
+    
         print(f'Converting frame {count}')
         
         # convert the image to grayscale
+
         grayscaleFrame = cv2.cvtColor(get(q1), cv2.COLOR_BGR2GRAY)
+            
         # Get next frame
         count += 1
     
@@ -58,8 +59,9 @@ def display(q2):
     count = 0
 
     while q2 is not None:
-        
+    
         frame = get(q2)
+        
         print(f'Displaying frame {count}')        
 
         # display the image in a window called "video" and wait 42ms
@@ -78,50 +80,47 @@ def display(q2):
 def put(queue,item):
 
     if queue == queue1:
-
-        print("We're in Q1 - PUT")
         emptyQ1.acquire()
-        mutex.acquire()
+        mutexQ1.acquire()
         queue.put(item)
-        mutex.release()
+        mutexQ1.release()
         fullQ1.release()
 
 
     else:
-        print("We're in Q2 - PUT")
         emptyQ2.acquire()
-        mutex.acquire()
+        mutexQ2.acquire()
         queue.put(item)
-        mutex.release()
+        mutexQ2.release()
         fullQ2.release()
         
 def get(queue):
 
     if queue == queue1:
-
-        print("We're in Q1 - GET")
-        emptyQ1.acquire()
-        mutex.acquire()
-        queue.get()
-        mutex.release()
-        fullQ1.release()
-
+        fullQ1.acquire()
+        mutexQ1.acquire()
+        im = queue.get()
+        mutexQ1.release()
+        emptyQ1.release()
+        
     else:
-        print("We're in Q2 - GET")
-        emptyQ2.acquire()
-        mutex.acquire()
-        queue.get()
-        mutex.release()
-        fullQ2.release()
-    
+        fullQ2.acquire()
+        mutexQ2.acquire()
+        im = queue.get()
+        mutexQ2.release()
+        emptyQ2.release()
+
+    return im
 #Bounded Semaphore ensures that there is a limit on the amount of stuff placed inside the queue
 #and that an empty queue is never read from   
 
-mutex = threading.Lock()
-emptyQ1 = threading.BoundedSemaphore(24)
-fullQ1 = threading.BoundedSemaphore(0)
-emptyQ2 = threading.BoundedSemaphore(24)
-fullQ2 = threading.BoundedSemaphore(0)
+mutexQ1 = threading.Lock()
+mutexQ2 = threading.Lock()
+
+emptyQ1 = threading.Semaphore(24)
+fullQ1 = threading.Semaphore(0)
+emptyQ2 = threading.Semaphore(24)
+fullQ2 = threading.Semaphore(0)
 
 fileName = 'clip.mp4'
 framesToLoad = 400
